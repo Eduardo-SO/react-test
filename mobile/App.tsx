b0fd403e2892dba6ee7fda541b4b875565cd53f3
Eduardo SO <eduardo.sool07@gmail.com>
2021-01-25 01:17:05 -0300

    Add ESLint, Prettier and Editor Config

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,14 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
 
-export default function App() {
+const App: React.FC = () => {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your appp!</Text>
       <StatusBar style="auto" />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -16,6 +15,8 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    justifyContent: 'center'
+  }
+})
+
+export default App
